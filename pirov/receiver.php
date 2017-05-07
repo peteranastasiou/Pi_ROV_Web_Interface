251564cb755cb762f9c9de54973ac6fe94e36a66
Peter Anastasiou <peter.anastasiou@gmail.com>
2017-05-07 18:21:41 +0930

    Keypresses submitted as motor commands

--- pirov/receiver.php
+++ pirov/receiver.php
@@ -1,6 +1,4 @@
 <?php
 $dat = $_POST['name'];
 echo "$dat";
-echo "->";
-echo shell_exec("echo $dat > output");
 echo shell_exec("echo $dat > /dev/shm/mjpeg/user_annotate.txt");
--- pirov/sub.js
+++ pirov/sub.js
@@ -1,41 +1,79 @@
 
-var lastCode = 0;
+var lastMsg = "--";
+var up = 0;
+var left = 0;
 
 $(document).keydown(function(event) {
     var code = event.keyCode;
-    // Skip key repeats:
-    if(lastCode == code)
-        return;
-    lastCode = code;
-    if(37 <= code && code <= 40) {
+    
+    if(37 <= code && code <= 40)
+    {
         event.preventDefault();
+        
+        if (code === 37 && left === 0) left = 1;
+        if (code === 38 && up === 0)   up = 1;
+        if (code === 39 && left === 0) left = -1;
+        if (code === 40 && up === 0)   up = -1;
+        
+        send(command(up, left));
     }
-    if (code === 37) // left arrow
-        send(" left ");
-    if (code === 38) // up arrow
-        send(" up ");
-    if (code === 39) // right arrow
-        send(" right ");
-    if (code === 40) // down arrow
-        send(" down ");
 });
 
 $(document).keyup(function(event) {
     var code = event.keyCode;
-    if (code === 37) // left arrow
-        send(" left_off ");
-    if (code === 38) // up arrow
-        send(" up_off ");
-    if (code === 39) // right arrow
-        send(" right_off ");
-    if (code === 40) // down arrow
-        send(" down_off ");
+    
+    if(37 <= code && code <= 40)
+    {
+        if (code === 38 || code === 40) up = 0;
+        if (code === 37 || code === 39) left = 0;
+        
+        send(command(up, left));
+    }
 });
 
+function command(up, left)
+{
+    if (up === 0)
+    {
+        if (left === 0)
+            return "--";  // standby
+        else if (left > 0)
+            return "v^";  // hard left
+        else
+            return "^v";  // hard right
+    }
+    else if (up > 0)
+    {
+        if (left === 0)
+            return "^^";  // forward
+        else if (left > 0)
+            return "-^";  // forward left
+        else
+            return "^-";  // forward right
+    }
+    else
+    {
+        if (left === 0)
+            return "vv";  // back
+        else if (left > 0)
+            return "v-";  // back left
+        else
+            return "-v";  // back right
+    }
+}
+
 function send(msg) {
-    $.post("pirov/receiver.php",
+    if(msg !== lastMsg)
+    {
+        //console.log("sent:"+msg);
+        lastMsg = msg;
+        // send to php on the pi via ajax
+        $.post("pirov/receiver.php",
             {name: msg},
+            // response from the pi
             function (data, status) {
                 console.log("data=["+data+"]");
-            });
+            }
+        );
+    }
 }
